b47f0e1e9a2054c22819e2e2baf95236480bdaf6
Austin <austin.meier@hotmail.com>
2016-08-31 18:39:30 -0400

    Added View button Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,6 +4,15 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
 
+     columns do
+       column do
+         panel "Quick Actions" do
+          div do
+            link_to('New Injury', new_admin_injury_path)
+            end
+          end
+        end
+      end
 
      columns do
        column do
@@ -11,7 +20,8 @@ ActiveAdmin.register_page "Dashboard" do
           table_for Injury.order('end_date asc').each do |injury|
               column(:first_name)
               column(:last_name)
-              column(:end_date)    {|injury| link_to(injury.end_date, admin_injury_path(injury)) }
+              column(:end_date)
+              column()             {|injury| link_to('View', admin_injury_path(injury)) }
             end
           end
          end
