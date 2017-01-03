3b959a507f5c23f98542e2bca944d1ad108b8a26
Austin <austin.meier@hotmail.com>
2017-01-03 12:55:40 -0500

    updated

--- db/migrate/20161013213227_create_athletes.rb
+++ db/migrate/20161013213227_create_athletes.rb
@@ -3,7 +3,10 @@ class CreateAthletes < ActiveRecord::Migration
     create_table :athletes do |t|
       t.string :first_name
       t.string :last_name
+      t.string :dob
       t.integer :grade
+      t.string :phone
+      t.string :address
       t.string :sport
     end
   end
