e302a7f317e6cbb2fe9d5ed587ca68342758e16c
7B Meier <7bmeiera@usa-mbpr-iloop.lexrich5.org>
2017-01-24 13:20:03 -0500

    sidebar

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -262,9 +262,6 @@ ActiveAdmin.setup do |config|
   # hand side with a filter for each attribute of the registered model.
   # You can enable or disable them for all resources here.
   #
-  config.before_filter do
-    right_sidebar!(collapsed: true)
-  end
   #
   # By default the filters include associations in a select, which means
   # that every record will be loaded for each association.
