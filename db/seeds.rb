c85045037e0c74cf0bb8d4bf9d08baee3d6c6801
Austin <austin.meier@hotmail.com>
2017-01-26 17:15:14 -0500

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,32 +33,32 @@ Treatment.create!([
   {athlete_id: 3, trainer_id: 1, treatment_location: "Shoulder", treatment_action: "Ice", comment: "Ice Pack", date: "2016-10-25", time: "01:05 PM"}
 ])
 Location.create!([
-  {location: "Other"}
-  {location: "Ankle"}
-  {location: "Arm"}
-  {location: "Back"}
-  {location: "Finger"}
-  {location: "Groin"}
-  {location: "Head"}
-  {location: "Hip"}
-  {location: "Knee"}
-  {location: "Abdomen"}
-  {location: "Shin"}
-  {location: "Shoulder"}
-  {location: "Thigh"}
-  {location: "Toe"}
+  {location: "Other"},
+  {location: "Ankle"},
+  {location: "Arm"},
+  {location: "Back"},
+  {location: "Finger"},
+  {location: "Groin"},
+  {location: "Head"},
+  {location: "Hip"},
+  {location: "Knee"},
+  {location: "Abdomen"},
+  {location: "Shin"},
+  {location: "Shoulder"},
+  {location: "Thigh"},
+  {location: "Toe"},
   {location: "Wrist"}
 ])
 Action.create!([
-  {action: "RICE"}
-  {action: "Cryotherapy"}
-  {action: "Compression"}
-  {action: "Crutches"}
-  {action: "Brace"}
-  {action: "Tape"}
-  {action: "US"}
-  {action: "ESTIM/NMES"}
-  {action: "Whirpool"}
-  {action: "Padding/Protection"}
+  {action: "RICE"},
+  {action: "Cryotherapy"},
+  {action: "Compression"},
+  {action: "Crutches"},
+  {action: "Brace"},
+  {action: "Tape"},
+  {action: "US"},
+  {action: "ESTIM/NMES"},
+  {action: "Whirpool"},
+  {action: "Padding/Protection"},
   {action: "NSAIDs"}
 ])
