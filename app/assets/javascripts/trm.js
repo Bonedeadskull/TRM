6f97bdb1570e3b0879cbead89055bfe1bd37220b
Austin <austin.meier@hotmail.com>
2017-01-26 13:25:55 -0500

    Toggle Filters button

--- app/assets/javascripts/trm.js
+++ app/assets/javascripts/trm.js
@@ -1,17 +1,20 @@
-var filterStatus = true, filters, table;
+var filterStatus = true, filters, table, button;
 
 window.onload = function() {
   filters = document.querySelector("#sidebar");
   table = document.querySelector("#main_content");
+  button = document.querySelector("#filter_button");
 }
 
 function toggleFilters() {
   if (filterStatus) {
     filters.style.display = "none";
     table.style['margin-right'] = "0";
+    button.innerText = "Show Filters";
   } else {
     filters.style.display = "";
     table.style['margin-right'] = "300px";
+    button.innerText = "Hide Filters";
   }
   filterStatus = !filterStatus;
 }
